cf30008f2c062d9914e78269a6a301cd59cf9096
kristie fisher <fisherkristie19@icloud.com>
2024-02-05 20:52:01 -0500

    testing and fixing bugged issues

--- index.js
+++ index.js
@@ -21,6 +21,26 @@ class LogoGenerator {
 
     fs.writeFileSync(filePath, svgContent, 'utf-8');
     console.log(`Generated ${filePath}`);
+}
+static generateLogo(text, textColor, shape, shapeColor) {
+    const svgContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+          <rect width="100%" height="100%" fill="${shapeColor}" />
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="20" fill="${textColor}">
+              ${text}
+          </text>
+      </svg>
+    `;
+
+    let filePath;
+    if (shape === 'general') {
+      filePath = 'examples/logo.svg';
+    } else {
+      filePath = `examples/example_${shape.toLowerCase()}.svg`;
+    }
+
+    fs.writeFileSync(filePath, svgContent, 'utf-8');
+    console.log(`Generated ${filePath}`);
   }
 
   static async main() {
